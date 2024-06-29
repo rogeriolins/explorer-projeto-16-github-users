b8cc1055588985b314872dc9cea0496ba2750360
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-06-29 10:52:56 -0300

    JS adjusting user github add

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,30 +1,63 @@
+export class GithubUser {
+  static search(userName) {
+    const endpointGitHub = `https://api.github.com/users/${userName}`
+    return fetch(endpointGitHub)
+      .then((data) => data.json())
+      .then((data) => ({
+        login: data.login,
+        name: data.name,
+        public_repos: data.public_repos,
+        followers: data.followers,
+      }))
+    // Tambem pode ser desestruturado:
+    //    ({login, name, public_repos, followers}) =>
+    //            ({login, name, public_repos, followers})
+  }
+}
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
+    // GithubUser.search("rogeriolins").then((user) => console.log(user))
   }
 
   load() {
-    this.entries = [
-      {
-        login: "maykbrito",
-        name: "Mayk Brito",
-        public_repos: "76",
-        followers: "120000",
-      },
-      {
-        login: "diego3g",
-        name: "Diego Fernandes",
-        public_repos: "96",
-        followers: "160000",
-      },
-      {
-        login: "rogeriolins",
-        name: "Rogerio Lins",
-        public_repos: "716",
-        followers: "240000",
-      },
-    ]
+    // this.entries = [
+    //   {
+    //     login: "maykbrito",
+    //     name: "Mayk Brito",
+    //     public_repos: "76",
+    //     followers: "120000",
+    //   },
+    //   {
+    //     login: "diego3g",
+    //     name: "Diego Fernandes",
+    //     public_repos: "96",
+    //     followers: "160000",
+    //   },
+    //   {
+    //     login: "rogeriolins",
+    //     name: "Rogerio Lins",
+    //     public_repos: "716",
+    //     followers: "240000",
+    //   },
+    // ]
+
+    const newEntries =
+      JSON.parse(localStorage.getItem("@github-favorites:")) || []
+    this.entries = newEntries
+  }
+
+  async add(userName) {
+    try {
+      const user = await GithubUser.search(userName)
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado!")
+      }
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -32,6 +65,9 @@ export class Favorites {
     const filteredEntries = this.entries.filter(
       (entry) => entry.login !== user.login
     )
+
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
@@ -40,6 +76,15 @@ export class FavoritesView extends Favorites {
     super(root)
     this.tbody = this.root.querySelector("table tbody")
     this.update()
+    this.onAdd()
+  }
+
+  onAdd() {
+    const addButton = this.root.querySelector(".search button")
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input")
+      this.add(value)
+    }
   }
 
   update() {
